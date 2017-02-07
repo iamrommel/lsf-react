4960e2c9d6bfd029b4c9873e5f9458e7b86a68c6
Rommel Manalo <manalo.rommel@gmail.com>
2017-02-07 17:56:36 +0800

    Publish new version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-export  class Library {
-  constructor() {
-    this._name = 'Library';
-  }
-  get name() {
-    return this._name;
-  }
+export class Library {
+    constructor() {
+      this._name = 'Library';
+    }
 
-  static log() {
-    console.log('this is shit men');
-  }
+    get name() {
+      return this._name;
+    }
+
+    static log() {
+      console.log('this is shit men');
+    }
 
 }
 
